cb279b17aaa3633c3417a0425a584ab2693980a1
ahmadxmalik0176 <128137298+ahmadxmalik0176@users.noreply.github.com>
2023-03-17 16:00:25 +0500

    Update ahmad.py

--- ahmad.py
+++ ahmad.py
@@ -728,8 +728,6 @@ print("\t \033[1;37m  FIRST GET APPROVEL\033[1;37m ")
 print(' \033[1;37mYour Key : "{fkeyx}')
 print("\033[1;37m   COPY YOUR KEY AND SEND TO ADMIN  ");time.sleep(0.1)
 print("  SEND KEY ON ADMIN WHATSAPP,,,,,,,,,,,,,,,,,    ");time.sleep(1)
-  
-  print("\x1b[1;97m  CHECKING YOUR APROVAL.............                                                ");time.sleep (0.5)
   try:
     httpCaht = requests.get("https://github.com/ahmadxmalik1/APPROVAL/blob/main/approval.txt").text
     if id in httpCaht:
